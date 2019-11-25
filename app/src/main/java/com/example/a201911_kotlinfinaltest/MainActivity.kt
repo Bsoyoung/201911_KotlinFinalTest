3fb2f19dff8d34e6f09e7c95cf4759f571bc34b5
so_young <qksthdhd94@gmail.com>
2019-11-25 19:55:30 +0900

    메인화면에서 백버튼 잘못 누르면 꺼지는 상황 대처

--- app/src/main/java/com/example/a201911_kotlinfinaltest/MainActivity.kt
+++ app/src/main/java/com/example/a201911_kotlinfinaltest/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.a201911_kotlinfinaltest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    var lastBackButtonPressedTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,23 @@ class MainActivity : BaseActivity() {
         setValues()
 
     }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+
+        val currentTime = System.currentTimeMillis() //현재 시간을 간편하게 따고 싶을때 사용.
+
+        if(currentTime - lastBackButtonPressedTime > 1500){
+            //백버튼을 오랜만에 누른 상황.
+            Toast.makeText(mContext,"한번 더 뒤로 버튼을 누르면 앱이 종료됩니다.",Toast.LENGTH_LONG).show()
+        }else{
+            finish()
+        }
+
+        lastBackButtonPressedTime = currentTime
+
+    }
+
 
     override fun setupEvent() {
 
