e0dfa6cc6423ff49690412dc283f9af04e6ee463
so_young <qksthdhd94@gmail.com>
2019-11-25 20:54:58 +0900

    내 마킹 숫자들을 ArrayList에 담아두기.

--- app/src/main/java/com/example/a201911_kotlinfinaltest/LottoActivity.kt
+++ app/src/main/java/com/example/a201911_kotlinfinaltest/LottoActivity.kt
@@ -11,6 +11,9 @@ class LottoActivity : BaseActivity() {
     var lottoNumArrayList = ArrayList<Int>()
     var thisWeekLottoNumTextViewArrayList = ArrayList<TextView>()
 
+    var myNumArrayList = ArrayList<Int>()
+    var myTextViewArrayList = ArrayList<TextView>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.lotto)
@@ -24,10 +27,24 @@ class LottoActivity : BaseActivity() {
 
             //숫자를 랜덤으로 6개 생성. 1~45 / 중복안됨.
             setThisWeekLottoNum()
-
+            checkLottoRank()
         }
 
     }
+
+    //당첨 결과를 체크. 몇 등인지 확인
+
+    fun checkLottoRank(){
+        //6개 : 1등 => 20억원
+        //5개 : 3등 => 150만원
+        //4개 : 4등 => 5만원
+        //3개 : 5등 => 5천원
+
+        //그 이하 : 꽝. => 0원
+
+
+    }
+
 
     fun setThisWeekLottoNum(){
 
@@ -101,6 +118,20 @@ class LottoActivity : BaseActivity() {
         thisWeekLottoNumTextViewArrayList.add(lottoNumTxt4)
         thisWeekLottoNumTextViewArrayList.add(lottoNumTxt5)
         thisWeekLottoNumTextViewArrayList.add(lottoNumTxt6)
+
+
+        myTextViewArrayList.add(myLottoNumTxt1)
+        myTextViewArrayList.add(myLottoNumTxt2)
+        myTextViewArrayList.add(myLottoNumTxt3)
+        myTextViewArrayList.add(myLottoNumTxt4)
+        myTextViewArrayList.add(myLottoNumTxt5)
+        myTextViewArrayList.add(myLottoNumTxt6)
+
+        for(myTv in myTextViewArrayList){
+            myNumArrayList.add(myTv.text.toString().toInt())
+        }
+
+
     }
 
 }
