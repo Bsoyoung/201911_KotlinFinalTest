c6cd1fa967bab2a320c75681152b7bcc22954ef1
so_young <qksthdhd94@gmail.com>
2019-11-26 20:49:13 +0900

    아이디 젖아 여부를 실제로 저장 / 출력

--- app/src/main/java/com/example/a201911_kotlinfinaltest/LoginActivity.kt
+++ app/src/main/java/com/example/a201911_kotlinfinaltest/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.a201911_kotlinfinaltest
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a201911_kotlinfinaltest.utils.ContextUtil
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
@@ -17,22 +18,14 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvent() {
+        savedIdCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            ContextUtil.setSavedIdChecked(mContext,isChecked)
+        }
+
     }
 
     override fun setValues() {
-
-        savedIdCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
-
-            if(isChecked){
-                //체크박스가 체크 되는 상황
-            }
-            else{
-
-            }
-
-
-
-        }
+        savedIdCheckBox.isChecked = ContextUtil.getSavedIdChecked(mContext)
 
 
     }
